98f47edf90a1e86ec9e7f0049b18cff8e0a0bb9a
Mufthi Ryanda <mufthi@diyo.app>
2023-06-29 21:06:34 +0700

    Add Example Usage

--- example/usage.go
+++ example/usage.go
@@ -1,5 +1,34 @@
 package example
 
-func Usage() {
+import (
+	"Go-Validation/validator"
+	"fmt"
+)
+
+func GetSingleError() {
+
+	type data struct {
+		Email          string `validation:"email"`
+		TermsOfService bool   `validation:"accepted"`
+	}
+
+	v := validator.NewValidator()
+
+	userData := data{
+		Email:          "mufthi@testing.com",
+		TermsOfService: true,
+	}
+
+	//Recommended to use pointer
+	// You can use v.Validate(userData) if you don't want to use pointer
+	err := v.Validate(&userData)
+
+	if err != nil {
+		errMsg := v.GetFirstError()
+		fmt.Println(errMsg)
+	}
+
+	//Validation passed
+	fmt.Println("OK")
 
 }
